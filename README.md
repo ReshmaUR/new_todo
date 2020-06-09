d37f0d0f95ab73e0dad8645994221f1c86147e37
ReshmaUR <reshsaradhi010@gmail.com>
2020-06-09 15:57:05 +0530

    updating files

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 "# new_todo" 
+click on the 'start' button in the home page
+Login by using username as 'admin' and password as '12345'.
+click on the 'To Do List' icon to view the list of tasks.
+after marking tasks that you have completed, click on the 'Done' button.
+the message will come as an alert.
+if you want to go back to login page ,click on the 'Logout' icon in the header. 
--- js/todo.js
+++ js/todo.js
@@ -1,14 +1,47 @@
+// login form validation callback concept 
+function validate(callback){
+
+    var user = document.getElementById("username").value;
+    var pass = document.getElementById("password").value;
+    var ruser = /^[a][d][m][i][n]$/;
+    var rpass = /^[1][2][3][4][5]$/;
+    if(ruser.test(user)){
+        if(rpass.test(pass)){
+            document.getElementById("err_pass").innerHTML = "";
+            // return true;
+            callback();
+        }
+    
+        else{
+            document.getElementById("err_user").innerHTML = "";
+            document.getElementById("err_pass").innerHTML = "Password is incorrect";
+            return false;
+        }
+    }
+    else{
+            document.getElementById("err_user").innerHTML = "Username is incorrect";
+            return false;
+        }
+    }
+    function callback(){
+        // return true;
+        // location.href = "main.html";
+        window.location.href ="main.html";
+    }
+
+
+
 // json to table
 $(document).ready(function(){
     $("#one").on("click",function(){
         $.ajax({
         url: 'https://jsonplaceholder.typicode.com/todos',
         dataType: 'json',
-        success: function(todo) {
-            for (var i=0; i<todo.length; i++) {
-                var row = $("<tr><td>" + todo[i].id+ "</td><td>" + todo[i].title+"</td><td>"+todo[i].completed+"</td><td>"+"<input type='checkbox'>"+"</td></tr>");
+        success: function(list) {
+            for (var i=0; i<list.length; i++) {
+                var row = $("<tr><td>" + list[i].id+ "</td><td>" + list[i].title+"</td><td>"+list[i].completed+"</td><td>"+"<input type='checkbox'>"+"</td></tr>");
                  
-                $('#myTable').append(row);
+                $('#dataTable').append(row);
             }
         },
         error: function(jqXHR, textStatus, errorThrown){
@@ -18,7 +51,9 @@ $(document).ready(function(){
     })
 });
 
-// checkbox validation
+
+
+// checkbox validation by promise
 function valcheck(){
     var promise = new Promise(function(resolve,reject){
         var check = document.querySelectorAll("input[type='checkbox']:checked").length;
@@ -42,88 +77,5 @@ function valcheck(){
 
 
 
-// login validation
-// function validate(){
-//     var user = document.getElementById("username").value;
-//     var pass = document.getElementById("password").value;
-//     var ruser = /^[a][d][m][i][n]$/;
-//     var rpass = /^[1][2][3][4][5]$/;
-//     if(ruser.test(user)){
-//         if(rpass.test(pass)){
-//             document.getElementById("err_pass").innerHTML = "";
-//             return true;
-//         }
-//         else{
-//             document.getElementById("err_user").innerHTML = "";
-//             document.getElementById("err_pass").innerHTML = "Password is incorrect";
-//             return false;
-//         }
-//     }
-//     else{
-//             document.getElementById("err_user").innerHTML = "Username is incorrect";
-//             return false;
-//         }
-// }
-
-// callback concept
-// function validate(callback){
-
-// var user = document.getElementById("username").value;
-// var pass = document.getElementById("password").value;
-// var ruser = /^[a][d][m][i][n]$/;
-// var rpass = /^[1][2][3][4][5]$/;
-// if()
 
 
-
-
-
-
-
-
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function validate(callback){
-//     var user = document.getElementById("username").value;
-//     var pass = document.getElementById("password").value;
-//     var ruser = /^[a][d][m][i][n]$/;
-//     var rpass = /^[1][2][3][4][5]$/;
-//     if(ruser.test(user)){
-//         alert("user ok");
-//         if(rpass.test(pass)){
-//             alert("password ok");
-//             // return true;
-//             callback();     
-//         }
-//         else{
-//             alert("password not ok"); 
-//             return false;
-//         }
-//     }
-//     else{
-//         alert("user not ok"); 
-//         return false;
-//     } 
-//     // callback();  
-// }
-// function callback(){
-//     // alert("all ok");
-//     return true;
-// }
-// validate(callback);
